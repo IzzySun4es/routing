263367caf554c177e9c7e3fba74905e24a9885a4
Alexandra Chugaenko <sascha281201@gmain.com>
2024-02-08 12:41:44 +0300

    12

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AboutPage from './components/aboutPage/aboutPage';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import MainPage from './components/mainPage/mainPage';
+import ProfilePage from './components/profilePage/profilePage'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+
+      <Routes>
+        <Route path='/' Component={MainPage}/>
+        <Route path='/profile' Component={ProfilePage}/>
+        <Route path='/about' Component={AboutPage}/>
+      </Routes> 
+
+    
+
+
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/aboutPage/aboutPage.jsx
@@ -0,0 +1,7 @@
+function AboutPage() {
+    return (<section>
+            <h1>About</h1>
+        </section>  );
+}
+
+export default AboutPage;
